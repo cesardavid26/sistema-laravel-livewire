cc8e3e1f52c7d86a79a6f1053715931a92c0500d
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-04-18 11:30:01 -0500

    subir imagenes

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -4,29 +4,40 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Post;
+use Livewire\WithFileUploads;
 
 class CreatePost extends Component
 {
+    use WithFileUploads;
+    
     public $open = true;
     
-    public $title, $content;
+    public $title, $content, $image, $identificador;
 
     public $rules = [
         'title' => 'required',
-        'content' => 'required'
+        'content' => 'required',
+        'image' => 'required|image|max:2048'
     ];
-
+public function mount(){
+    $this -> identificador=rand();
+}
    
     public function save(){
 
         $this -> validate();
 
+        $image = $this -> image->store('posts');
+
         Post::create([
             'title' => $this->title,
-            'content' => $this->content
+            'content' => $this->content,
+            'image' => $image
 
         ]);
-        $this->reset(['open', 'title', 'content']);
+        $this->reset(['open', 'title', 'content', 'image']);
+
+        $this->identificador = rand();
 
         $this->emitTo('show-posts','render');
         $this->emit('alert', 'El post se creo satisfactoriamente!');
